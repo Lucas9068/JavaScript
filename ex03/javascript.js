41da060dbf01c751e220af0b07243b3fe3bad694
Lucas9068 <104735509+Lucas9068@users.noreply.github.com>
2022-05-17 19:42:38 -0300

    Adicionando teste lógico para nova condição
    Adicionada nova condição para contagem regressiva do valor inicial para o final.

--- ex03/javascript.js
+++ ex03/javascript.js
@@ -6,15 +6,20 @@ function calc() {
     let n1 = Number(init.value)
     let n2 = Number(end.value)
     let n3 = Number(step.value)
-    if (init.value.length == 0 || n2 < n1 || n2 == 0) {
+    if (init.value.length == 0 || n2 == 0) {
         alert("[ERRO!] Preencha corretamente os campos indicados.")
         res.innerHTML = `Impossível contar. );`
     } else if (step.value.length == 0 || n3 == 0) {
         alert('[ERRO!] Passo com valor inválido, valor será reajustado para 1.')
         n3 = 1
-    }
-    res.innerHTML = `Contando:<br>`
-    for (n1; n1<=n2; n1+=n3) {
-        res.innerHTML += `-> ${n1}`
+        res.innerHTML = `Contando:<br>`
+        for (n1; n1 <= n2; n1 += n3) {
+            res.innerHTML += `-> ${n1}`
+        }
+    } else if (n2 < n1) {
+        res.innerHTML = `Contando:<br>`
+        for (n1; n2 <= n1; n1 -= n3) {
+            res.innerHTML += `-> ${n1}`
+        }
     }
 }
